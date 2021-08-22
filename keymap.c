8b04b89c84e6d3a7206a7aeba6741b3dd450824a
mtoohey31 <mtoohey31@users.noreply.github.com>
2021-08-22 17:50:06 -0400

    fix: unusable mods on num layer

--- keymap.c
+++ keymap.c
@@ -39,9 +39,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_NUM] = LAYOUT_planck_grid(
     KC_LBRC, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_TILD, KC_SLSH, KC_7,    KC_8,    KC_9,    KC_MINS, KC_RBRC,
-    _______, KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_GRV,  KC_ASTR, KC_4,    KC_5,    KC_6,    KC_PLUS, KC_AMPR,
+    _______, KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_GRV,  KC_ASTR, KC_4,    KC_5,    KC_6,    KC_PLUS, KC_APP,
     _______, KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_HASH, KC_PERC, KC_1,    KC_2,    KC_3,    KC_EQL,  _______,
-    _______, _______, _______, KC_CIRC, KC_AT,   KC_DLR,  DF(_NUM), KC_0,   DF(_QWERTY), _______, _______, _______
+    _______, _______, _______, _______, _______, _______, DF(_NUM), KC_0,   DF(_QWERTY), _______, _______, _______
 ),
 
 [_FPS] = LAYOUT_planck_grid(
